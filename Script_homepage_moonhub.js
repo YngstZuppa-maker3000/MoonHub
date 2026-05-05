673ed2be2186efd0f371b097a8a98de225e5526d
ElVettorato <gabricarle2008@gmail.com>
2026-05-05 12:51:08 +0200

    Make vectorial images, fix the particles background, and make hamburger menu

--- Script_homepage_moonhub.js
+++ Script_homepage_moonhub.js
@@ -3,27 +3,53 @@ const btnRandom = document.getElementById("btnRandomGames");
 const btnSinglePlayer = document.getElementById("btnSinglePlayerGames");
 const btnTwoPlayers = document.getElementById("btnTwoPlayerGames");
 
-btnRandom.addEventListener('click', function() {
-    window.location.href = "./RandomGames_folder/Index_RG.html";
-});
-btnSinglePlayer.addEventListener('click', function() {
-    window.location.href = "./SinglePlayerGames_folder/Index_SPG_page1.html";
-});
-btnTwoPlayers.addEventListener('click', function() {
-    window.location.href = "./TwoPlayerGames_folder/Index_TPG_page1.html";
-});
+if (btnRandom) {
+    btnRandom.addEventListener('click', function() {
+        window.location.href = "./RandomGames_folder/Index_RG.html";
+    });
+}
+if (btnSinglePlayer) {
+    btnSinglePlayer.addEventListener('click', function() {
+        window.location.href = "./SinglePlayerGames_folder/Index_SPG_page1.html";
+    });
+}
+if (btnTwoPlayers) {
+    btnTwoPlayers.addEventListener('click', function() {
+        window.location.href = "./TwoPlayerGames_folder/Index_TPG_page1.html";
+    });
+}
 
 //Button Movement section
 const btnMoveRG = document.getElementById("btnMovementRG");
 const btnMoveSPG= document.getElementById("btnMovementSPG");
 const btnMoveTPG= document.getElementById("btnMovementTPG");
 
-btnMoveRG.addEventListener('click', function() {
+function toggleSideMenu() {
+    const sideMenu = document.getElementById("sideMenu");
+    const menuOverlay = document.getElementById("menuOverlay");
+    if (sideMenu && menuOverlay) {
+        if (sideMenu.classList.contains("open")) {
+            sideMenu.classList.remove("open");
+            menuOverlay.classList.remove("active");
+            setTimeout(() => { 
+                if(!sideMenu.classList.contains("open")) menuOverlay.style.display = "none"; 
+            }, 500);
+        } else {
+            menuOverlay.style.display = "block";
+            // Trigger reflow
+            void menuOverlay.offsetWidth;
+            sideMenu.classList.add("open");
+            menuOverlay.classList.add("active");
+        }
+    }
+}
 
-});
-btnMoveSPG.addEventListener('click', function() {
+if (btnMoveRG) btnMoveRG.addEventListener('click', toggleSideMenu);
+if (btnMoveSPG) btnMoveSPG.addEventListener('click', toggleSideMenu);
+if (btnMoveTPG) btnMoveTPG.addEventListener('click', toggleSideMenu);
 
-});
-btnMoveTPG.addEventListener('click', function() {
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+if (closeMenuBtn) closeMenuBtn.addEventListener('click', toggleSideMenu);
 
-});
+const menuOverlay = document.getElementById("menuOverlay");
+if (menuOverlay) menuOverlay.addEventListener('click', toggleSideMenu);
