0bcd615f28f92dd73185f59f6ef7869512617c53
YngstZuppa-maker3000 <mcapriello08@gmail.com>
2026-05-05 08:46:59 +0200

    Fixed div

--- Script_homepage_moonhub.js
+++ Script_homepage_moonhub.js
@@ -3,11 +3,11 @@ const btnSinglePlayer = document.getElementById("btnSinglePlayerGames");
 const btnTwoPlayers = document.getElementById("btnTwoPlayerGames");
 
 btnRandom.addEventListener('click', function() {
-    window.location.href = "Index_RG.html";
+    window.location.href = "./RandomGames_folder/Index_RG.html";
 });
 btnSinglePlayer.addEventListener('click', function() {
-    window.location.href = "Index_SPG_page1.html";
+    window.location.href = "./SinglePlayerGames_folder/Index_SPG_page1.html";
 });
 btnTwoPlayers.addEventListener('click', function() {
-    window.location.href = "Index_TPG_page1.html";
+    window.location.href = "./TwoPlayerGames_folder/Index_TPG_page1.html";
 });
